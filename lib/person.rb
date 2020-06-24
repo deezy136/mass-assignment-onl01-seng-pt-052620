57685113caa55a74d36d0e539cfb85588f7ca466
Clyde Griffin <deezy13@gmail.com>
2020-06-24 16:44:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   #your code here
+  
+  attr_accessor :name, :hair_color
+  
+  def initialize(persons_attributes)
+      
+      persons_attributes.each do | att, v| 
+        
+        
+        
+        
+  end 
 end
