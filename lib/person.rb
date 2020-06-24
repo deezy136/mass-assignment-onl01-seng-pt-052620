7beeda2b9670102fea55841cc7ab337c5ef27977
Clyde Griffin <deezy13@gmail.com>
2020-06-24 16:47:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,9 @@
 class Person
   #your code here
   
-  attr_accessor :name, :hair_color
+  attr_accessor :name, :hair_color: :birthday, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(persons_attributes)
       
@@ -13,12 +15,4 @@ class Person
         
         
   end 
-end
-
-class User
-  attr_accessor :name, :user_name, :age, :location, :bio
- 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
 end
